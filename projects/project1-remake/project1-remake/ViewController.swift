9f13863428a4824611754aeb182df65729aa1bf1
Mohammad Eid <mohammad.mossad.eid@gmail.com>
2024-01-20 14:00:43 +0200

    Show pictures name list

--- projects/project1-remake/project1-remake/ViewController.swift
+++ projects/project1-remake/project1-remake/ViewController.swift
@@ -7,11 +7,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UITableViewController {
+    var pictures = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let fileManager = FileManager()
+        let path = Bundle.main.resourcePath!
+        let contents = try! fileManager.contentsOfDirectory(atPath: path)
+        
+        for content in contents {
+            if content.hasPrefix("nssl") {
+                pictures.append(content)
+            }
+        }
+        
+        print(pictures)
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture")!
+        cell.textLabel?.text = pictures[indexPath.row]
+        return cell
     }
 
 
