b7ed43b0e597009eca69e7d7cb5bb8e3c93e7059
Mohammad Eid <mohammad.mossad.eid@gmail.com>
2024-01-20 13:45:33 +0200

    started project1 remake

---
+++ projects/project1-remake/project1-remake/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  project1-remake
+//
+//  Created by Mohammad Eid on 20/01/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
