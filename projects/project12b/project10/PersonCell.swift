5ffc05b027d1092b90aa5db5440c83a16be000c4
Mohammad Eid <mohammad.mossad.eid@gmail.com>
2024-02-04 18:27:42 +0200

    Initialized project 12

---
+++ projects/project12b/project10/PersonCell.swift
@@ -0,0 +1,14 @@
+//
+//  PersonCell.swift
+//  project10
+//
+//  Created by Mohammad Eid on 02/02/2024.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+    
+}
