fcacaeee0c86da8708a6b1ef3481de4f0d69b640
Mohammad Eid <mohammad.mossad.eid@gmail.com>
2024-02-05 18:58:55 +0200

    completed milestorne project 4 (Day 50)

---
+++ projects/milestone-project4/milestone-project4/AppImage.swift
@@ -0,0 +1,8 @@
+//
+//  AppImage.swift
+//  milestone-project4
+//
+//  Created by Mohammad Eid on 05/02/2024.
+//
+
+import Foundation
---
+++ projects/milestone-project4/milestone-project4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  milestone-project4
+//
+//  Created by Mohammad Eid on 05/02/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
